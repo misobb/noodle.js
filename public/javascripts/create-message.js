069d51dcb39ccf7b315c3c795092bc996383e7d6
speedyop <speedyop@yahoo.com>
2011-03-24 22:39:15 +0100

    message form contractwq

---
+++ public/javascripts/create-message.js
@@ -0,0 +1,21 @@
+$('[data-role=page]').live('pageshow', function() {
+  $("#message-textarea").html("send a message");
+  $("#message-textarea").focus(function() {
+    $(this).html("");
+    $(this).removeClass("contract");
+    $(this).addClass("expand");
+    $(this).removeClass("no-text");
+    $(this).addClass("filled");
+    $("#create-message .ui-btn").show();
+  });
+  $("#message-textarea").focusout(function() {
+    if ($(this).val() == "") {
+      $(this).html("send a message");
+      $(this).removeClass("filled");
+      $(this).addClass("no-text");
+    }
+    $(this).removeClass("expand");
+    $(this).addClass("contract");
+    $("#create-message .ui-btn").hide();
+  });
+});
